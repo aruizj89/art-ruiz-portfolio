6b4b35ae88fc26d5d34a4370c39e555fe09a3955
Art Ruiz <a.ruiz.jr@gmail.com>
2022-04-25 00:12:14 -0500

    Initialized MFE module `Portfolio`

---
+++ webpack/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3003,
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "PORTFOLIO",
+      filename: "remoteEntry.js",
+      exposes: {},
+      remotes: {
+        SHELL: "SHELL@http://localhost:3001/remoteEntry.js",
+      },
+      shared: [
+        {
+          ...deps,
+          react: { requiredVersion: deps.react, singleton: true },
+          "react-dom": {
+            requiredVersion: deps["react-dom"],
+            singleton: true,
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+};
